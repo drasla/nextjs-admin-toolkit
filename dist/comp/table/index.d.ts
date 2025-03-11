231a1d6b77999bf55823dff338a3b0d87a31d39c
d3v_friends <d3v.friends@gmail.com>
2025-03-11 15:27:23 +0900

    0.0.8 add Table.List

--- dist/comp/table/index.d.ts
+++ dist/comp/table/index.d.ts
@@ -1,7 +1,9 @@
 import Click from "../table/click";
 import Link from "../table/link";
+import List from "../table/list";
 declare const _default: {
     Link: typeof Link;
     Click: typeof Click;
+    List: typeof List;
 };
 export default _default;
--- src/comp/table/index.ts
+++ src/comp/table/index.ts
@@ -1,4 +1,5 @@
 import Click from "@comp/table/click";
 import Link from "@comp/table/link";
+import List from "@comp/table/list";
 
-export default {Link, Click};
+export default {Link, Click, List};
